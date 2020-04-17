84c7d89702eb69e4c7f5f18971cd946bc12face4
linlinsongyun <37888350+linlinsongyun@users.noreply.github.com>
2020-04-17 18:40:44 +0800

    Create run.py

---
+++ run.py
@@ -0,0 +1,13 @@
+import sys
+import os
+wav_dir = sys.argv[1]
+txt_name = sys.argv[2]
+save_dir = sys.argv[3]
+
+world_dir = os.apth.join(save_dir, 'world')
+ark_dir = os.path.join(save_dir, 'ppgs')
+ppgs_recover_dir = os.path.join(save_dir, 'ppgs_recover')
+ppgs251_dir = os.path.join(save_dir, 'ppgs251')
+ppgs_thre = os.path.joiin(save_dir, 'ppgs_090')
+
+os.system
